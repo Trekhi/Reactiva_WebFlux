02b2e199f4c815680f18af024cb3812eaa4fd91d
Trekh <danieltamriv@gmail.com>
2024-10-17 18:39:14 -0500

    first commit

---
+++ config/src/test/java/com/tamara/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamara.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
